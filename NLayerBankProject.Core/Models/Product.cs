a837f40c09340d8d2fc531f2bc1de8b79241fcfa
kereemyildiz <a.keremyildiz99@gmail.com>
2021-07-26 12:03:08 +0300

    Add project files.

---
+++ NLayerBankProject.Core/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerBankProject.Core.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+        public virtual Category Category { get; set; } // in order to entity framework can track.
+
+
+    }
+}
